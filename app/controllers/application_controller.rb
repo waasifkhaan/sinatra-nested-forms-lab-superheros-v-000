fab8eeba76089e62d6c42acd429d33ae9a902158
Wasif Khan <waasif.khaan@gmail.com>
2018-09-01 23:39:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :team
+    end
+    
+    post '/teams' do 
+      
+      
+      erb :super_hero 
+      
+    end 
+
+
+   
+  
 
 end
